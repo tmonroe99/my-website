49e9423e8efbeca0dbd7661cd5d0206be84f1296
taylor younkins <tayloryounkins@pal-nat186-164-80.itap.purdue.edu>
2019-08-26 14:48:53 -0400

    first commit, nav bar?

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import NavBar from './Components/NavBar'
+// import 'react-table' from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="App" >
+      <NavBar></NavBar>
+      {/* <header className="App-header">
+      hi
+      <Button variant="contained" color="primary">CLick me</Button>
+      </header> */}
     </div>
   );
 }
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import AboutMe from './AboutMe';
+
+
+const StyledTabs = withStyles({
+  indicator: {
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+    '& > div': {
+      maxWidth: 40,
+      width: '100%',
+      backgroundColor: '#635ee7',
+    },
+  },
+})(props => <Tabs {...props} TabIndicatorProps={{ children: <div /> }} />);
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <Typography
+        component="div"
+        color="#ffffff"
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        <Box p={3}>{children}</Box>
+      </Typography>
+    );
+  }
+
+function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  }
+
+const StyledTab = withStyles(theme => ({
+  root: {
+    textTransform: 'none',
+    color: '#fff',
+    fontWeight: theme.typography.fontWeightRegular,
+    fontSize: theme.typography.pxToRem(15),
+    marginRight: theme.spacing(1),
+    '&:focus': {
+      opacity: 1,
+    },
+  },
+}))(props => <Tab disableRipple {...props} />);
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  padding: {
+    padding: theme.spacing(3),
+  },
+  demo1: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  demo2: {
+    backgroundColor: '#73123f',
+  },
+}));
+
+
+
+export default function CustomizedTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  function handleChange(event, newValue) {
+    setValue(newValue);
+  }
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.demo2}>
+        <StyledTabs value={value} onChange={handleChange} aria-label="styled tabs example">
+          <StyledTab label="About Me" />
+          <StyledTab label="Resume" />
+          <StyledTab label="Portfolio" />
+          <StyledTab label="Commissions" />
+          <StyledTab label="Contact" />
+        </StyledTabs>
+        <Typography className={classes.padding} />
+        <TabPanel value={value} index={0}>
+            <AboutMe/>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+      Resume
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Portfolio 
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+      Commissions 
+      </TabPanel>
+      <TabPanel value={value} index={4}>
+      Contact 
+      </TabPanel>
+      </div>
+    </div>
+  );
+}
