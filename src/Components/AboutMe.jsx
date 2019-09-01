40c0d1bc1968ee1002e397fa865a47e365a9c14c
taylor younkins <tayloryounkins@taylors-MacBook-Pro.local>
2019-09-01 15:37:17 -0400

    card changes

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Box from '@material-ui/core/Box';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import Container from '@material-ui/core/Container';
-import Styling from './styling/aboutme.scss';
+import { Box, Paper, Grid, Container, Card } from '@material-ui/core';
+import styling from './styling/aboutme.scss';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -21,16 +18,21 @@ export default function AboutMe () {
     const classes = useStyles();
     return (
         <div>
-            <h3>who am i?</h3>
-            <Container className="text">
-            I've always been both a creative and technical person. 
-            I've been programming, doing graphic design, and editing videos since middle school.
-             Doing web development was something I always thought about doing, but I only recently 
-             gained experience which really had me draw another connection between my technical and creative skills. I wanted a place to 
-             showcase these technical and creative skills that I enjoy cultivating. I'm a very visual oriented person and I'm really excited to also
-             combined my After Effects, Photoshop, and Illustrator skills to create even more enticing design!
-            </Container>
 
+            <Container className={styling.card}>
+              <Card className={styling.card}>
+              <h3>who am i?</h3>
+              <div className="card">
+              I've always been both a creative and technical person. 
+              I've been programming, doing graphic design, and editing videos since middle school.
+              Doing web development was something I always thought about doing, but I only recently 
+              gained experience which really had me draw another connection between my technical and creative skills. I wanted a place to 
+              showcase these technical and creative skills that I enjoy cultivating. I'm a very visual oriented person and I'm really excited to also
+              combined my After Effects, Photoshop, and Illustrator skills to create even more enticing design!
+              </div>
+
+              </Card>
+            </Container>
         </div>
     
     );
--- src/Components/Commissions.jsx
+++ src/Components/Commissions.jsx
@@ -1,19 +1,24 @@
-import Typography from '@material-ui/core/Typography';
-import React from 'react';
-import styles from './styling/commissions.scss';
+import React from 'react';  
+import { Typography, Card } from '@material-ui/core';
+import styling from './styling/commissions.scss';
+
 
 export default function Commissions(){
 
     return (
-        <p className="text">
-            Contact me directly for details.<br/><br/>
-            Small Commissions: Open <br/>
-            - simplistic kinetic Typography<br/>
-            - concise scope<br/>
-            Large Commissions: Currently Unavailable (Taken)<br/>
-            - Trailers<br/>
-            - Heavy visual elements<br/>
-            - High production<br/>
-        </p>
+        <div>
+            <Card className="card">
+                <p className={styling.text}>
+                Contact me directly for details.<br/><br/>
+                    Small Commissions: Open <br/>
+                    - simplistic kinetic Typography<br/>
+                    - concise scope<br/>
+                    Large Commissions: Currently Unavailable (Taken)<br/>
+                    - Trailers<br/>
+                    - Heavy visual elements<br/>
+                    - High production<br/>
+                </p>
+            </Card>
+        </div>
     );
 }
