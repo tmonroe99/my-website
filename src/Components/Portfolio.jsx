792c15f0562ebe797131ab8b940c8072c607b605
taylor younkins <tayloryounkins@pal-nat186-173-133.itap.purdue.edu>
2019-09-18 14:35:37 -0400

    test

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -6,7 +6,7 @@ import ReactPlayer from 'react-player'
 import { Component } from 'react';
 import styles from './styling/portfolio.scss';
 
-  
+
 const useStyles = makeStyles(theme => ({
     root: {
       flexGrow: 1,
@@ -40,6 +40,52 @@ export default function Portfolio() {
     return (
     <div>
         <section class="grid-1">
+        <div class="item-1">
+          <Card>
+              <Paper>
+            <ReactPlayer url='https://vimeo.com/228564766' paused />
+            </Paper>
+            </Card>
+          </div>
+          <div class="item-2">
+          <Card className="cardtest">
+            OPSPARC Contest promotion explaining what NASA spinoffs are, targeted to K-12 students.<br/>
+            Summer 2017 <br/>
+            Program: Adobe After Effects 
+            </Card>
+          </div>
+
+          <div class="item-1">
+          <Card>
+              <Paper>
+            <ReactPlayer url='https://vimeo.com/273628224' paused />
+            </Paper>
+            </Card>
+          </div>
+          <div class="item-2">
+          <Card className="cardtest">
+            Part of my role as a Student Assistant for Professor Harriger.<br/>
+             January - May 2018 <br/>
+            Program: Adobe After Effects 
+            </Card>
+          </div>
+
+
+          <div class="item-1">
+            <Card>
+              <Paper>
+            <ReactPlayer url='https://www.youtube.com/watch?v=7LPQe26Knxs' paused />
+            </Paper>
+            </Card>
+          </div>
+          <div class="item-2">
+            <Card className="cardtest">
+            Motion graphics music video for Lollia's cover of the Persona 5 opening. <br/>
+            Program: Adobe After Effects  <br/>
+            2018 
+            </Card>
+          </div>
+
           <div class="item-1">
               <Card>
                 <Paper>
@@ -70,50 +116,10 @@ export default function Portfolio() {
             </Card>
           </div>
 
-          <div class="item-1">
-            <Card>
-              <Paper>
-            <ReactPlayer url='https://www.youtube.com/watch?v=7LPQe26Knxs' paused />
-            </Paper>
-            </Card>
-          </div>
-          <div class="item-2">
-            <Card className="cardtest">
-            Motion graphics music video for Lollia's cover of the Persona 5 opening. <br/>
-            Program: Adobe After Effects  <br/>
-            2018 
-            </Card>
-          </div>
 
-          <div class="item-1">
-          <Card>
-              <Paper>
-            <ReactPlayer url='https://vimeo.com/273628224' paused />
-            </Paper>
-            </Card>
-          </div>
-          <div class="item-2">
-          <Card className="cardtest">
-            Part of my role as a Student Assistant for Professor Harriger.<br/>
-             January - May 2018 <br/>
-            Program: Adobe After Effects 
-            </Card>
-          </div>
+
           
-          <div class="item-1">
-          <Card>
-              <Paper>
-            <ReactPlayer url='https://vimeo.com/228564766' paused />
-            </Paper>
-            </Card>
-          </div>
-          <div class="item-2">
-          <Card className="cardtest">
-            OPSPARC Contest promotion explaining what NASA spinoffs are, targeted to K-12 students.<br/>
-            Summer 2017 <br/>
-            Program: Adobe After Effects 
-            </Card>
-          </div>
+
       </section>
       <h3>For more, visit my Vimeo: <Link>https://vimeo.com/tayloryounkins</Link> </h3>
       </div>
